885c11aef20dc8063c7a5f63dd4a62011af4571c
Abrar Hasnath <160423735304@mjcollege.ac.in>
2026-08-17 13:17:10 +0530

    Create testbench.v

---
+++ lab-1/testbench.v
@@ -0,0 +1,20 @@
+module and_gate_tb; 
+reg A; 
+reg B; 
+wire out; 
+and_gate_design uut(.A(A), .B(B), .out(out));
+initial begin 
+$display (" time=%t A = %0b| B =%0b |out =%b",$time,A,B, out);
+$monitor (" time=%t A = %0b| B =%0b|out = %b",$time,A,B,out); 
+#10; 
+end 
+A = 1'b0; B = 1'b0 ; #10; 
+A = 1'b0; B = 1'b1 ; #10; 
+A = 1'b1; B = 1'b0 ; #10; 
+A = 1'b1; B = 1'b1 ; #10; 
+$finish; 
+initial begin 
+$dumpfile("and_gate_dump.vcd");
+  $dumpvars(); 
+end 
+endmodule
